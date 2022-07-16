bcf6242adecf0d3aaf49c1f8ccabfb9a21653a92
Elsever <106514920+Elsever-Emirli@users.noreply.github.com>
2022-07-16 18:04:17 +0400

    deployment

---
+++ api/app.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql2')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+require('dotenv').config()
+
+const db = mysql.createConnection({
+    user: process.env.NAME,
+    host: process.env.HOST,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    insecureAuth : true,
+})
+
+
+db.connect((err)=>{
+    if(err){console.log(err)};
+    console.log('Connet');
+
+
+app.post('/login',(req,res)=>{
+    const name = req.body.name
+    const password = req.body.password
+    
+        
+        db.query("SELECT * FROM login WHERE name = ? AND password = ? ;",[name,password],(err,results)=>{
+            if(err){ res.send({err:err}) };
+           
+                       
+            if(results.length == 0){
+                res.send({message : "Wrong Password or Name"})
+            }else{
+                res.send(results)
+            }
+        })
+       
+})
+
+
+
+app.post('/register',(req,res)=>{
+    const name = req.body.name
+    const password = req.body.password
+    
+        
+        db.query("INSERT INTO login (name,password) VALUES( ?, ?);",[name,password],(err,results)=>{
+            if(results){console.log(results)};
+            res.send(err)
+            
+        })
+     
+})
+
+app.post('/addpost',(req,res)=>{
+    const PostName = req.body.PostName
+    const PostPhotoUrl = req.body.PostPhotoUrl
+    const PostPlacer = req.body.PostPlacer
+    const PostAbout = req.body.PostAbout
+    const PostFullAbout = req.body.PostFullAbout
+
+    db.query('INSERT INTO addpost (PostName,PostPhotoUrl,PostPlacer,PostAbout,PostFullAbout) VALUES( ?,?,?,?,?);',[PostName,PostPhotoUrl,PostPlacer,PostAbout,PostFullAbout],(err,results)=>{
+        if(err){res.send(err)}
+        if(results){res.send(results)};
+    })
+})
+
+
+app.get('/post',(req,res)=>{
+    db.query('SELECT * FROM addpost',(err,result)=>{
+        if(err){res.send(err)}
+        if(result){res.send(result)}
+    })
+})
+
+
+}) 
+
+
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3002
+
+app.listen(PORT,()=>{
+    console.log("Run")
+})
---
+++ api/client/src/pages/Post.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function Post() {
+
+  const [posts,setPosts] = useState([])
+
+  useEffect(()=>{
+    axios.get('/post')
+    .then((response)=>{setPosts(response.data)})
+  })
+
+  return (
+    <div>
+                {/* <!-- Container for demo purpose --> */}
+          <div className="container my-24 px-6 mx-auto">
+
+            {/* <!-- Section: Design Block --> */}
+            <section className="mb-32 text-gray-800 text-center">
+
+              <h2 className="text-3xl font-bold mb-12 pb-4 text-center">Latest articles</h2>
+              <div className="grid lg:grid-cols-3 gap-6 xl:gap-x-12">
+
+
+
+
+              {
+                posts?.map((post,index)=>{
+                  return(
+                                  
+                <div className="mb-0" key={index} >
+                <div className="relative block bg-white rounded-lg shadow-lg">
+                  <div className="flex">
+                    <div
+                      className="relative overflow-hidden bg-no-repeat bg-cover relative overflow-hidden bg-no-repeat bg-cover shadow-lg rounded-lg mx-4 -mt-4"
+                      data-mdb-ripple="true" data-mdb-ripple-color="light">
+                      <img src={post.PostPhotoUrl} className="w-full" />
+                      <a href="#!">
+                        <div
+                          className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
+                          style={{backgroundColor: "rgba(251, 251, 251, 0.15)"}}></div>
+                      </a>
+                    </div>
+                  </div>
+                  <div className="p-6">
+                    <h5 className="font-bold text-lg mb-3">{post.PostName}</h5>
+                    <p className="text-gray-500 mb-4">
+                      <small> by
+                        <a href="" className="text-gray-900">{post.PostPlacer}</a></small>
+                    </p>
+                    <p className="mb-4 pb-2">
+                      {post.PostAbout}
+                    </p>
+                    <a href={post.PostFullAbout} data-mdb-ripple="true" data-mdb-ripple-color="light"
+                      className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Read
+                      more</a>
+                  </div>
+                </div>
+              </div>
+              )
+
+                })
+              }
+
+
+
+
+              </div>
+
+
+              
+
+            </section>
+            {/* <!-- Section: Design Block --> */}
+
+          </div>
+    </div>
+  )
+}
+
+export default Post
