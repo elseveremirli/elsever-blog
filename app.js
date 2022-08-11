25ecb8837dfaf24b2afe48aca55066614f876d0a
Elsever <106514920+Elsever-Emirli@users.noreply.github.com>
2022-08-11 21:52:45 +0400

    ewf

--- app.js
+++ app.js
@@ -80,11 +80,11 @@ app.get('/post',(req,res)=>{
 }) 
 
 
-// app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/client/build/public', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3002
 
