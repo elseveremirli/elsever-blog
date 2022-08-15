444c10f70aee0b4a8ca8454c70776fc7f15b4546
Elsever <106514920+Elsever-Emirli@users.noreply.github.com>
2022-08-15 15:49:08 +0400

    go

--- app.js
+++ app.js
@@ -82,8 +82,8 @@ app.get('/post',(req,res)=>{
 
 app.use(express.static(path.join(__dirname, "/client/build")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/public', 'index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3002
